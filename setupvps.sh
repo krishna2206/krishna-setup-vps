5735bdc2d7cc714f54f8cf77b43a02ca8f195f24
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-10-13 18:28:12 +0300

    added setup oh-my-zsh zsh-syntax-highlighting plugin

--- setupvps.sh
+++ setupvps.sh
@@ -10,4 +10,5 @@ sudo apt install -y exa htop curl wget git zsh tmux neofetch micro
 sudo chsh -s $(which zsh)
 sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp .zshrc ~/.zshrc
