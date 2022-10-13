2d57e9c225ff938ea08b91fe7912c3a211dab77a
Fitiavana Krishna <fitiavana.krishna@gmail.com>
2022-10-13 18:21:08 +0300

    Initial commit

---
+++ setupvps.sh
@@ -0,0 +1,13 @@
+# Asume that the current system is Ubuntu 22.04
+# Update the repositories and upgrade the system
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install the basic packages
+sudo apt install -y exa htop curl wget git zsh tmux neofetch micro
+
+# Setup the zsh and oh-my-zsh
+sudo chsh -s $(which zsh)
+sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cp .zshrc ~/.zshrc
