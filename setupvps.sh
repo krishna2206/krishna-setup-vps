96105c405dbce45787d32791dae2e00ae97c8fc5
ANHY Krishna Fitiavana <fitiavana.krishna@gmail.com>
2022-10-14 09:37:24 +0300

    Update setupvps.sh

--- setupvps.sh
+++ setupvps.sh
@@ -8,7 +8,7 @@ sudo apt install -y exa htop curl wget git zsh tmux neofetch micro
 
 # Setup the zsh and oh-my-zsh
 sudo chsh -s $(which zsh)
-sh -c \"$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp .zshrc ~/.zshrc
